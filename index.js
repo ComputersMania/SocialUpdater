d3733e187b2ac17369eb24a4bbcd8b6e456c79ba
ComputersMania <vincenzp3393@gmail.com>
2018-02-28 23:58:26 +0100

    History to validate posts

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-let config = require('./config.json');
+config = require('./config.json')
 
-// Loading dependencies
+// Loading rss-parser and creating an in instance of it
 
-Parser = require('rss-parser');
-
-// Creating istances
-
-parser = new Parser;
+Parser = require('rss-parser')
+parser = new Parser
 
 // Loading db
+
+storage = require('node-persist')
+storage.initSync({dir: 'db'})
+
+// Creating array on first boot
+
+if (storage.values().length == 0) {
+  storage.setItemSync('history', [])
+}
 
 // Routine to post to social networks
 
 postEverywhere = function(item) {
   console.log(item.title + ' ' + item.link)
-};
+}
+
+// function to confirm that the post isn't too old and hasn't been publicized yet
+
+validate = function(item) {
+  return (storage.getItemSync('history').indexOf(item.link) == -1)
+}
 
 // main function
 
 main = function() {
   parser.parseURL(config.feedUrl).then(function(feed) {
     feed.items.forEach(function(item) {
-      postEverywhere(item)
-    });
-  });
-};
+      if (validate(item)) {
+        postEverywhere(item)
+        history = [item.link].concat(storage.getItemSync('history'))
+        storage.setItemSync('history', history)
+      }
+    })
+  })
+}
 
 // autorun on load
 
