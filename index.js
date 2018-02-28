f877a4b77188da6a308485c07aca9674af0d6487
ComputersMania <vincenzp3393@gmail.com>
2018-02-28 22:42:34 +0100

    Something starts working

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-var config = require('./config.json')
+let config = require('./config.json');
+
+// Loading dependencies
+
+Parser = require('rss-parser');
+
+// Creating istances
+
+parser = new Parser;
+
+// Loading db
+
+// Routine to post to social networks
+
+postEverywhere = function(item) {
+  console.log(item.title + ' ' + item.link)
+};
+
+// main function
+
+main = function() {
+  parser.parseURL(config.feedUrl).then(function(feed) {
+    feed.items.forEach(function(item) {
+      postEverywhere(item)
+    });
+  });
+};
+
+// autorun on load
+
+main()
+
+// exposing a webhook to trigger main
