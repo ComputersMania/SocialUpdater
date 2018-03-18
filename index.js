cd26c436bc41fc4cd7374f4ccf7e812fde222b49
ComputersMania <vincenzp3393@gmail.com>
2018-03-18 20:21:07 +0100

    added time validation

--- index.js
+++ index.js
@@ -19,16 +19,12 @@ if (storage.values().length == 0) {
 
 // Check if Google api key is set
 if (typeof process.env.GOOGLE_API_KEY !== 'undefined') {
-
   // Load googleapis
   const {google} = require('googleapis')
-
   // Load the urlshorten google api endpoint
   const urlshortener =  google.urlshortener('v1')
-
   // Create a promise from it
   const shortenerpromise = promisify(urlshortener.url.insert)
-
   // Async function that returns shortened link
   var shorten = async (link) => {
     try {
@@ -103,11 +99,16 @@ if (secret.instagramId && secret.instagramSecret) {
   instangram = require('instagram')
 }
 
-// function to confirm that the post isn't too old and hasn't been publicized yet
-
+// Validation function
 let validate = async (item) => {
+  // Load the array of previously posted items
   let history = await storage.getItem('history')
-  return (history.indexOf(item.link) == -1)
+  //Check time elapsed from pubblication
+  let pubDate = new Date(item.pubDate)
+  let now = new Date()
+  let elapsed = now.getTime() - pubDate.getTime()
+
+  return (history.indexOf(item.link) == -1 && elapsed < 1000*24*60*60)
 }
 
 // main function
@@ -121,10 +122,11 @@ let main = () => {
             if ( typeof(twitter) !== 'undefined' ) { postTwitter(item) }
             if (typeof shorten !== 'undefined') {
               try {
-                shorten(item.link).then(console.log)} // Just as a demostration/Will be removed soon
+                shorten(item.link).then(console.log) // Just as a demostration/Will be removed soon
               } catch(err) {
                 console.error(err)
               }
+            }
             history = [item.link].concat(storage.getItemSync('history'))
             storage.setItemSync('history', history)
           }
