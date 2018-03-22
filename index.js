2442f7ba4f1f088fc62668223a22d8bbdf81cd9b
lesuitesaltorrione <ilgiardinodimelange@gmail.com>
2018-03-22 23:23:09 +0100

    Twitter working

--- index.js
+++ index.js
@@ -17,24 +17,33 @@ if (storage.values().length == 0) {
   storage.setItemSync('history', [])
 }
 
-// Creating shorten function
+// Check if Google api key is set
+if (typeof process.env.GOOGLE_API_KEY !== 'undefined') {
 
-const {promisify} = require('util')
-const {google} = require('googleapis')
+  // Load googleapis
+  const {google} = require('googleapis')
 
-const urlshortener =  google.urlshortener('v1')
+  // Load the urlshorten google api endpoint
+  const urlshortener =  google.urlshortener('v1')
 
-// I like promises
+  // Create a promise from it
+  const shortenerpromise = promisify(urlshortener.url.insert)
 
-const shortenerpromise = promisify(urlshortener.url.insert)
-
-let shorten = (link) => {
-  return shortenerpromise({
-    key: secret.googleApiKey,
-    resource: {
-      longUrl: link
+  // Async function that returns shortened link
+  var shorten = async (link) => {
+    try {
+      var item = await shortenerpromise({
+        key: process.env.GOOGLE_API_KEY,
+        resource: {
+          longUrl: link
+        }
+      })
+    } catch(err) {
+      throw err
     }
-  })
+    return item.data.id
+  }
+  console.log('Google shortner enabled')
 }
 
 // Custom campaign link builder
@@ -65,30 +74,34 @@ let postFacebook = async (item) => {
   if (secret.googleApiKey) {
     link = await gapiUrl.shortenURL()
   }
-  
+
 }
 
 // Loading Twitter and istancing it
 
-if (secret.twitterKey && secret.twitterSecret && secret.twitterToken) {
-  Twitter = require('twitter')
-  twitter = new Twitter({
-    consumer_key: secret.twitterSecret,
-    consumer_secret: secret.twitterSecret,
-    bearer_token: secret.twitterToken
+if (typeof process.env.TWITTER_KEY !== 'undefined') {
+  const Twitter = require('twitter')
+  let twitter = new Twitter({
+    consumer_key: process.env.TWITTER_KEY,
+    consumer_secret: process.env.TWITTER_SECRET,
+    access_token_key: process.env.TWITTER_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_TOKEN_SECRET
   })
+  var postTwitter = (item) => {
+    status = 'A new post about ' + item.title + '. Read it here ' + item.link
+    twitter.post('statuses/update', {
+      status: status
+    })
+  }
 }
 
 // Function to post to twitter
 
-let postTwitter = (item) => {
-
-}
 
 // Loading instagram and spawning an instance
 
 if (secret.instagramId && secret.instagramSecret) {
-  instangram = require('instagram')
+   const instangram = require('instagram')
 }
 
 // Routine to post to social networks
@@ -98,27 +111,38 @@ let postEverywhere = (item) => {
   // Post on facebook
   // if (fb) { postFacebook(item) }
   // Post on Twitter
-  // if (twitter) { postTwitter(item) }
+  if (typeof postTwitter) { postTwitter(item) }
 }
 
 // function to confirm that the post isn't too old and hasn't been publicized yet
 
-let validate = (item) => {
-  return (storage.getItemSync('history').indexOf(item.link) == -1)
+let validate = async (item) => {
+  // Load the array of previously posted items
+  let history = await storage.getItem('history')
+  //  Check time elapsed from pubblication
+  let pubDate = new Date(item.pubDate)
+  let now = new Date()
+  let elapsed = now.getTime() - pubDate.getTime()
+  // Returns True if the item is good
+  return (history.indexOf(item.link) == -1 && elapsed < 1000*24*60*60)
 }
 
 // main function
 
 let main = () => {
-  parser.parseURL(config.feedUrl).then((feed) => {
+  parser.parseURL(config.feedUrl)
+  .then( feed => {
     feed.items.forEach((item) => {
-      if (validate(item)) {
+      validate(item).then( valid => { if (valid) {
+        console.log(item.title + ' ' + item.link)
+        if (typeof postTwitter !== 'undefined') { postTwitter(item) }
         postEverywhere(item)
         history = [item.link].concat(storage.getItemSync('history'))
         storage.setItemSync('history', history)
       }
-    })
+    })})
   })
+  .catch(error => console.log(error) )
 }
 
 // autorun on load
